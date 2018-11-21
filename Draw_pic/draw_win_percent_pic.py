98dd3fec3dc0bc53099ab346a7688b4d482501ea
GREEK_VICTOR <PastoreMaxwell@outlook.com>
2018-11-21 22:36:03 +0800

    filter zero

--- Draw_pic/draw_win_percent_pic.py
+++ Draw_pic/draw_win_percent_pic.py
@@ -11,7 +11,7 @@ import pandas as pd
 from matplotlib.ticker import MultipleLocator, FormatStrFormatter
 from matplotlib.ticker import FuncFormatter
 
-data=pd.read_excel(r'C:\Users\vanPersie\Desktop\etar.xlsx',sheet_name='Sheet3')
+data=pd.read_excel(r'C:\Users\Pasto\Desktop\etar.xlsx',sheet_name='Sheet3')
 data.rename(columns={'赛事':'A','独赢':'B','全场 - 让球':'C','全场 - 大小':'D','单双':'E','胜负':'F'},inplace=True)
 data=data[['A','B','C','D','E','F']]
 all=data['F'][data.F>=-1].value_counts()
@@ -65,6 +65,7 @@ ccc={
     'b':kkk
 }
 xintu=pd.DataFrame(ccc)
+xintu=xintu[['a','b']][xintu.b!=0]
 print(xintu)
 '''
 去掉0点
